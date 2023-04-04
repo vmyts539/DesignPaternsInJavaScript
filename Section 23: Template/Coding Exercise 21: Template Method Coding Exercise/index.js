d4d74660db684d76b686358178d0967928275ea4
Vasyl Mytskaniuk <vasul2537125@gmail.com>
2023-04-04 22:05:25 +0300

    Finish Section 23: Template

---
+++ Section 23: Template/Coding Exercise 21: Template Method Coding Exercise/index.js
@@ -0,0 +1,51 @@
+class Creature {
+  constructor(attack, health) {
+    this.attack = attack;
+    this.health = health;
+  }
+}
+
+class CardGame {
+  constructor(creatures) {
+    this.creatures = creatures;
+  }
+
+  // returns index of winner if there's a winner
+  // returns -1 if there's no winner (both alive or both dead)
+  combat(creature1index, creature2index) {
+    let first = this.creatures[creature1index];
+    let second = this.creatures[creature2index];
+    this.hit(first, second);
+    this.hit(second, first);
+    let firstAlive = first.health > 0;
+    let secondAlive = second.health > 0;
+    if (firstAlive === secondAlive) return -1;
+    return firstAlive ? creature1index : creature2index;
+  }
+
+  hit(attacker, defender) {
+    throw new Error("Please implement this in inheritors");
+  }
+}
+
+class TemporaryCardDamageGame extends CardGame {
+  constructor(creatures) {
+    super(creatures);
+  }
+
+  hit(attacker, defender) {
+    let oldHealth = defender.health;
+    defender.health -= attacker.attack;
+    if (defender.health > 0) defender.health = oldHealth;
+  }
+}
+
+class PermanentCardDamageGame extends CardGame {
+  constructor(creatures) {
+    super(creatures);
+  }
+
+  hit(attacker, defender) {
+    defender.health -= attacker.attack;
+  }
+}
