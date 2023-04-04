d83af92cae6c581793ddc3a71bb58d750e75d123
Vasyl Mytskaniuk <vasul2537125@gmail.com>
2023-04-04 09:53:15 +0300

    Finish Section 19: Memento

---
+++ Section 19: Memento/Coding Exercise 17: Memento Coding Exercise/index.js
@@ -0,0 +1,32 @@
+class Token {
+  constructor(value = 0) {
+    this.value = value;
+  }
+}
+
+class Memento {
+  constructor() {
+    this.tokens = [];
+  }
+}
+
+class TokenMachine {
+  constructor() {
+    this.tokens = [];
+  }
+
+  addTokenValue(value) {
+    return this.addToken(new Token(value));
+  }
+
+  addToken(token) {
+    this.tokens.push(token);
+    let m = new Memento();
+    m.tokens = this.tokens.map((t) => new Token(t.value));
+    return m;
+  }
+
+  revert(m) {
+    this.tokens = m.tokens.map((t) => new Token(t.value));
+  }
+}
